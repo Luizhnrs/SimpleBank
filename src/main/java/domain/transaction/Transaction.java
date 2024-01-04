266a5d7371d957378161b048fe56065af00c7b96
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-04 14:53:53 -0300

    Correction: Import Client

--- src/main/java/domain/transaction/Transaction.java
+++ src/main/java/domain/transaction/Transaction.java
@@ -1,16 +1,26 @@
 package domain.transaction;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity(name = "transaction")
 @Table(name = "transaction")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Transaction {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal value;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private C
 }
