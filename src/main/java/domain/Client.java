91a15f17b15ae777d3a1c6a8dc7d6e0dc0ac86a0
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-04 17:16:31 -0300

    Feat: Lombok Implements

--- src/main/java/domain/Client.java
+++ src/main/java/domain/Client.java
@@ -1,9 +1,15 @@
 package domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity(name = "clients")
+@Table(name = "clients")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
