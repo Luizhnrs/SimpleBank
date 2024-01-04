00aa752801bfb6ceb6c94bcb502322f6f3beee6e
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-04 16:27:55 -0300

    Feat: Excepetion to Entrepreneur

--- src/main/java/com/example/SimpleBank/services/ClientService.java
+++ src/main/java/com/example/SimpleBank/services/ClientService.java
@@ -1,5 +1,21 @@
 package com.example.SimpleBank.services;
 
-public class ClientService {
+import com.example.SimpleBank.domain.client.Client;
+import com.example.SimpleBank.domain.client.ClientType;
+import com.example.SimpleBank.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
+@Service
+public class ClientService {
+    @Autowired
+    private ClientRepository repository;
+
+    public void validateTransaction(Client sender, BigDecimal value) throws Exception {
+        if(sender.getClientType() == ClientType.ETREPREUNEUR){
+            throw new Exception("This entrepreneur tier user is not able to make this transaction");
+        }
+    }
 }
