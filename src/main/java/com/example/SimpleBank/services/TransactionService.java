93c6591738c04c3fa5d190c215b7183f3ab7d55b
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-04 17:30:56 -0300

    Feat: Create Transaction

--- src/main/java/com/example/SimpleBank/services/TransactionService.java
+++ src/main/java/com/example/SimpleBank/services/TransactionService.java
@@ -1,5 +1,6 @@
 package com.example.SimpleBank.services;
 
+import com.example.SimpleBank.domain.client.Client;
 import com.example.SimpleBank.domain.transaction.Transaction;
 import com.example.SimpleBank.dtos.TransactionDTO;
 import com.example.SimpleBank.repository.TransactionRepository;
@@ -14,7 +15,8 @@ public class TransactionService {
     @Autowired
     private TransactionRepository repository;
 
-    public void createTransaction (TransactionDTO transaction) {
-
+    public void createTransaction (TransactionDTO transaction) throws Exception {
+        Client sender = this.clientService.findClientById(transaction.senderId());
+        Client receiver = this.clientService.findClientById(transaction.receiverId());
     }
 }
