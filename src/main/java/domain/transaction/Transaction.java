d61da5cafb89dacc4a2b3e68d769832c71aa3ff7
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-04 17:32:52 -0300

    Feat: Transaction Lombok Import

--- src/main/java/domain/transaction/Transaction.java
+++ src/main/java/domain/transaction/Transaction.java
@@ -2,9 +2,15 @@ package domain.transaction;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "transaction")
 @Table(name = "transaction")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
 
 }
