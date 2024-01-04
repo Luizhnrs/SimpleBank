9d047c43e30c79c71c01740560bebe333bf86528
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-04 17:32:21 -0300

    Feat: Transaction Imp

---
+++ src/main/java/domain/transaction/Transaction.java
@@ -0,0 +1,10 @@
+package domain.transaction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity(name = "transaction")
+@Table(name = "transaction")
+public class Transaction {
+
+}
