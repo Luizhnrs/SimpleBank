eb2a965d977497c8a884f0beab1972d4f9c8e9b8
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-05 12:44:25 -0300

    Feat: Client Controller config new Client

--- src/main/java/com/example/SimpleBank/controllers/ClientController.java
+++ src/main/java/com/example/SimpleBank/controllers/ClientController.java
@@ -3,6 +3,7 @@ package com.example.SimpleBank.controllers;
 import com.example.SimpleBank.domain.client.Client;
 import com.example.SimpleBank.dtos.ClientDTO;
 import com.example.SimpleBank.services.ClientService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientController {
     @PostMapping
     public ResponseEntity<Client> createClient(ClientDTO client){
-        Client newClient = clientService.createClient(client);
+        Client newClient = ClientService.createClient(client);
+        return new ResponseEntity<>(newClient, HttpStatus.CREATED);
     }
 }
