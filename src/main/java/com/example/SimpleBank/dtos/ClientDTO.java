03613a0da975976b5ff9661fd5a861105627b498
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-05 15:20:44 -0300

    Correction: DTO Correction

--- src/main/java/com/example/SimpleBank/dtos/ClientDTO.java
+++ src/main/java/com/example/SimpleBank/dtos/ClientDTO.java
@@ -4,5 +4,5 @@ import com.example.SimpleBank.domain.client.ClientType;
 
 import java.math.BigDecimal;
 
-public record ClientDTO(String name, String surname, BigDecimal balance, String email, String password, ClientType clientType) {
+public record ClientDTO(String name, String surname, String document, BigDecimal balance, String email, String password, ClientType clientType) {
 }
