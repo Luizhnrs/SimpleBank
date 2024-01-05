95b530b7d43e0465f37fc0c77d2f327b39ab6fe9
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-05 12:49:58 -0300

    Correction: Create Client in controller

--- src/main/java/com/example/SimpleBank/controllers/ClientController.java
+++ src/main/java/com/example/SimpleBank/controllers/ClientController.java
@@ -3,6 +3,7 @@ package com.example.SimpleBank.controllers;
 import com.example.SimpleBank.domain.client.Client;
 import com.example.SimpleBank.dtos.ClientDTO;
 import com.example.SimpleBank.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/clients")
 public class ClientController {
+    @Autowired
+    private ClientService clientService;
     @PostMapping
     public ResponseEntity<Client> createClient(ClientDTO client){
-        Client newClient = ClientService.createClient(client);
+        Client newClient = clientService.createClient(client);
         return new ResponseEntity<>(newClient, HttpStatus.CREATED);
     }
 }
