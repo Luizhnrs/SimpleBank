eee5aea6d5a91d045f3c11865d5149e2e084b447
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-05 15:20:28 -0300

    Feat: Annotation Import

--- src/main/java/com/example/SimpleBank/controllers/ClientController.java
+++ src/main/java/com/example/SimpleBank/controllers/ClientController.java
@@ -6,13 +6,12 @@ import com.example.SimpleBank.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/clients")
+@RestController()
+@RequestMapping("/clients")
 public class ClientController {
     @Autowired
     private ClientService clientService;
