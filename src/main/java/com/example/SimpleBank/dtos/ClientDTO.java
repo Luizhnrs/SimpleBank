3c048907b475c2190347038d810dae21b05aee5a
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-05 12:45:10 -0300

    Feat: DTO Implement

--- src/main/java/com/example/SimpleBank/dtos/ClientDTO.java
+++ src/main/java/com/example/SimpleBank/dtos/ClientDTO.java
@@ -1,6 +1,8 @@
 package com.example.SimpleBank.dtos;
 
+import com.example.SimpleBank.domain.client.ClientType;
+
 import java.math.BigDecimal;
 
-public record ClientDTO(String name, String surname, BigDecimal balance, String email, String password) {
+public record ClientDTO(String name, String surname, BigDecimal balance, String email, String password, ClientType clientType) {
 }
