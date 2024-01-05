68af0de474874bf8be4740aeeb87bc4b7e515962
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-05 11:55:34 -0300

    Feat: Message Response

--- src/main/java/com/example/SimpleBank/services/MessageService.java
+++ src/main/java/com/example/SimpleBank/services/MessageService.java
@@ -1,7 +1,9 @@
 package com.example.SimpleBank.services;
 
 import com.example.SimpleBank.domain.client.Client;
+import com.example.SimpleBank.dtos.MessageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,5 +14,11 @@ public class MessageService {
 
     public void sendMessage(Client client, String message){
         String email = client.getEmail();
+        MessageDTO messageRequest = new MessageDTO(email, message);
+
+
+        ResponseEntity <String> messageResponse = restTemplate.postForEntity("https://3l531.wiremockapi.cloud/", messageRequest, String.class);
     }
+
+
 }
